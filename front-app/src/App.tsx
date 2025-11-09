bc7be927c725f75f5a1f51b4be4808146a28132c
Yoshihito Nakata <tenten.yoshihito@gmail.com>
2025-11-10 02:13:41 +0900

    動作確認

--- front-app/src/App.tsx
+++ front-app/src/App.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         first commit
-        </p>
+        <p>動作確認</p>
         <a
           className="App-link"
           href="https://reactjs.org"
