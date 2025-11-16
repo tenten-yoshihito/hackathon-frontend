81a398c1f7647fcd41a58b88b29087b09489b074
Yoshihito Nakata <tenten.yoshihito@gmail.com>
2025-11-16 18:41:07 +0900

    dbを削除

--- front-app/src/firebase/firebaseConfig.ts
+++ front-app/src/firebase/firebaseConfig.ts
@@ -13,12 +13,11 @@ const firebaseConfig = {
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGEING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const fireAuth = getAuth(app);
-export const db = getFirestore(app);
-export default app;
+export default app; 
